3fb2d561c85c2bae693edf1ba365d512ca003ac1
Abhay Parashar <52857736+Abhayparashar31@users.noreply.github.com>
2022-04-25 13:28:10 +0530

    Update home.py

--- home.py
+++ home.py
@@ -5,7 +5,12 @@ def app():
     st.subheader(''' A **Multipurpose Web App** That Can- ''')
     st.write(''' #### 1. Predict The First Inning Score Based On Current Situation of The Match.''')
     st.write(''' #### 2. Provide A Winner Probability For Any Condition of Second Innings of the Match.''')
-    st.write(''' #### 3. Provide Quick Analysis Between Teams, Players,etc.  ''')
+    st.write(''' #### 3. Detailed Exploratory Data Analysis On Dataset  ''')
+    st.write(''' #### 4. Team vs Team Analysis  ''')
+    st.write(''' #### 5. Player vs Player Analysis  ''')
+    st.write(''' #### 6. Team Record Analysis  ''')
+    st.write(''' #### 7. Player Career Analysis As Batsman and Bowler  ''')
+    
     
     st.write(''' \n''')
     st.write('---')
