a49164176fc2c624c95f9a0f447a83fa2d4a6a09
Abhay Parashar <52857736+Abhayparashar31@users.noreply.github.com>
2022-06-26 12:04:34 +0530

    Update home.py

--- home.py
+++ home.py
@@ -2,8 +2,8 @@ import streamlit as st
 def app():
     st.markdown(
         '''
-        # IPL MATCH PREDICTIONS AND ANALYSIS
-        '''
+        <h1 style='text-align: center; color: white; font-weight:blod;'>IPL MATCH PREDICTIONS AND ANALYSIS</h1>"
+        ''', unsafe_allow_html=True
     )
     st.image('main_app_IPL_img.jpg',use_column_width='always')
     st.markdown(
