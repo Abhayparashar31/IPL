5ee44d25cd98cda81cf7e5dfe7f0547543d2a57f
Abhay Parashar <52857736+Abhayparashar31@users.noreply.github.com>
2022-06-26 11:57:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,19 @@ import player
 import team
 
 import streamlit as st
+st.set_page_config(
+    page_title = "IPL ANALYSIS",
+    page_icon = "🏏",
+    initial_sidebar_state='expanded',
+)
 
+st.markdown(
+    """
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    """,
+    unsafe_allow_html=True,
+)
 
 PAGES = {
     "HOME":home,
