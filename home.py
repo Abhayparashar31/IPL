551ef7bedb4d1f6918566e4f8bcb33384a75e015
Abhay Parashar <52857736+Abhayparashar31@users.noreply.github.com>
2022-06-26 12:11:48 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,10 +1,11 @@
 import streamlit as st
+
 def app():
     st.set_page_config(
-    page_title = "IPL ANALYSIS",
-    page_icon = "🏏",
-    initial_sidebar_state='expanded',
-    layout="centered"
+        page_title = "IPL ANALYSIS",
+        page_icon = "🏏",
+        initial_sidebar_state='expanded',
+        layout="centered"
     )
     st.markdown(
         '''
