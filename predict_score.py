636b6346e5d829d025c167a2139f668f17ff814b
Abhay Parashar <52857736+Abhayparashar31@users.noreply.github.com>
2022-07-06 09:50:15 +0530

    Update predict_score.py

--- predict_score.py
+++ predict_score.py
@@ -32,50 +32,54 @@ def app():
         batting_team = st.selectbox('Batting Team At The Moment',random.choices(TEAMS,k = len(TEAMS))) ## randomizing choices
     with col2:
         bowling_team = st.selectbox('Bowling Team At The Moment',random.choices(TEAMS,k = len(TEAMS)))
-    
-    encoded_batting_team = [1 if batting_team==TEAM else 0 for TEAM in TEAMS]
-    encoded_bowling_team = [1 if bowling_team==TEAM else 0 for TEAM in TEAMS]
-    
-    ### Current Runs
-    current_runs = st.number_input('Enter Current Score of Batting Team..')
+      
+    if bowling_team==batting_team:
+        st.error("Bowling and Batting Team Can't Be Same")
+    else:
 
-    ### Wickets Out
-    wickets_left = st.number_input('Enter Number of Wickets Left For Batting Team..')
-    wickets_out = 10-wickets_left
+        encoded_batting_team = [1 if batting_team==TEAM else 0 for TEAM in TEAMS]
+        encoded_bowling_team = [1 if bowling_team==TEAM else 0 for TEAM in TEAMS]
 
-    ## Overs Spent
-    over = st.number_input('Current Over of The Match..')
+        ### Current Runs
+        current_runs = st.number_input('Enter Current Score of Batting Team..')
 
-    ## Runs In Last 5
-    run_lst_5 = st.number_input('How Many Runs Batting Team Has Scored In Last 5 Overs ?')
+        ### Wickets Out
+        wickets_left = st.number_input('Enter Number of Wickets Left For Batting Team..')
+        wickets_out = 10-wickets_left
 
-    ## Wickets In Last 5
-    wicket_lst_5 = st.number_input('Number of  Wickets Taken By Bowling Team In The Last 5 Overs ?')
-    
+        ## Overs Spent
+        over = st.number_input('Current Over of The Match..')
 
-    ### DATA
-    # st.write(batting_team,bowling_team)
-    # st.write(encoded_batting_team)
-    # st.write(encoded_bowling_team)
-    # st.write(current_runs)
-    # st.write(wickets_out)
-    # st.write(over)
-    # st.write(run_lst_5)
-    # st.write(wicket_lst_5)
+        ## Runs In Last 5
+        run_lst_5 = st.number_input('How Many Runs Batting Team Has Scored In Last 5 Overs ?')
 
-    data = [int(current_runs),int(wickets_out),over,int(run_lst_5),int(wicket_lst_5)]
-    data.extend(encoded_batting_team)
-    data.extend(encoded_bowling_team)
-    st.write('---')
-    st.write('Encoded Input Data:',pd.DataFrame([data]))
-    ### Generating Predictions
-    Generate_pred = st.button("Predict Score")
-    if Generate_pred:
-
-        pred = model.predict([data])
-        st.subheader(f'The Predicted Score Will Be Between {int(pred)-5} - {int(pred)+5}')
+        ## Wickets In Last 5
+        wicket_lst_5 = st.number_input('Number of  Wickets Taken By Bowling Team In The Last 5 Overs ?')
 
 
+        ### DATA
+        # st.write(batting_team,bowling_team)
+        # st.write(encoded_batting_team)
+        # st.write(encoded_bowling_team)
+        # st.write(current_runs)
+        # st.write(wickets_out)
+        # st.write(over)
+        # st.write(run_lst_5)
+        # st.write(wicket_lst_5)
 
-   
-    
+        data = [int(current_runs),int(wickets_out),over,int(run_lst_5),int(wicket_lst_5)]
+        data.extend(encoded_batting_team)
+        data.extend(encoded_bowling_team)
+        st.write('---')
+        st.write('Encoded Input Data:',pd.DataFrame([data]))
+        ### Generating Predictions
+        Generate_pred = st.button("Predict Score")
+        if Generate_pred:
+
+            pred = model.predict([data])
+            st.subheader(f'The Predicted Score Will Be Between {int(pred)-5} - {int(pred)+5}')
+
+
+
+
+
